8c7046ffcf7dd83f4e7018ca4f85c9d28fb42ad2
Sajal satsangi <sajalsatsangi2004@gmail.com>
2024-02-01 17:37:37 +0530

    Home Page Completed

---
+++ src/components/Home/index.js
@@ -0,0 +1,65 @@
+import './index.scss'
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['a', 'j', 'a', 'l'] 
+    const surnameArray = ['a', 't', 's', 'a', 'n', 'g', 'i']
+    const jobArray = [
+        'w',
+        'e',
+        'b',
+        ' ',
+        'd',
+        'e',
+        'v',
+        'e',
+        'l',
+        'o',
+        'p',
+        'e',
+        'r',
+        '.',
+    ]
+
+    useEffect(() => {
+        return setTimeout (() => {
+            setLetterClass('text-animate-hover')
+        },4000)
+    }, [])
+
+    return (
+        <div className='container home-page'>
+            <div className='text-zone'>
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br/>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt='developer' />
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={15} />
+                    <img src={LogoTitle} alt='developer' />
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={surnameArray}
+                    idx={19} />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    idx={26} />
+                </h1>
+                <h2>Frontened Developer/ UI Designer/ React-Native Developer</h2>
+                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo />
+        </div>
+    )
+}
+
+export default Home
