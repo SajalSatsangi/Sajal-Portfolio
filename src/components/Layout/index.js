f7daed060778af2aeb05c2615b5f6a4edc75de3c
Sajal satsangi <sajalsatsangi2004@gmail.com>
2024-02-11 09:00:32 +0530

    cursor update

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,58 +4,60 @@ import { motion } from 'framer-motion';
 import './index.scss';
 import { useEffect, useState } from 'react';
 import DarkMode from "./DarkMode/DarkMode";
+import MouseParticles from 'react-mouse-particles'
 
 const Layout = () => {
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0
-  });
+  // const [mousePosition, setMousePosition] = useState({
+  //   x: 0,
+  //   y: 0
+  // });
 
-  useEffect(() => {
-    const mouseMove = e => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY
-      });
-    };
+  // useEffect(() => {
+  //   const mouseMove = e => {
+  //     setMousePosition({
+  //       x: e.clientX,
+  //       y: e.clientY
+  //     });
+  //   };
 
-    window.addEventListener("mousemove", mouseMove);
+  //   window.addEventListener("mousemove", mouseMove);
 
-    return () => {
-      window.removeEventListener("mousemove", mouseMove);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("mousemove", mouseMove);
+  //   };
+  // }, []);
 
-  const variants = {
-    default: {
-      x: mousePosition.x,
-      y: mousePosition.y
-    }
-  };
+  // const variants = {
+  //   default: {
+  //     x: mousePosition.x,
+  //     y: mousePosition.y
+  //   }
+  // };
 
   return (
     <>
-    <div className="App">
+      <div className="App">
 
-      <DarkMode />
-      <Sidebar />
-      <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
+        <DarkMode />
+        <Sidebar />
+        <div className="page">
+          <span className="tags top-tags">&lt;body&gt;</span>
 
-        <Outlet />
-        <span className="tags bottom-tags">
-          &lt;/body&gt;
-          <br />
-          <span className="bottom-tag-html">&lt;/html&gt;</span>
-        </span>
+          <Outlet />
+          <span className="tags bottom-tags">
+            &lt;/body&gt;
+            <br />
+            <span className="bottom-tag-html">&lt;/html&gt;</span>
+          </span>
+        </div>
+
+        {/* <motion.div
+          className='cursor'
+          variants={variants}
+          animate="default"
+        /> */}
+        <MouseParticles g={1} num={6} color="random" cull="stats,image-wrapper" level={6} />
       </div>
-
-      <motion.div
-        className='cursor'
-        variants={variants}
-        animate= "default"
-      />
-    </div>
     </>
   );
 };
