2d198b87bdb25b28389f21351fc534cea42f4a58
rashmi-singh2003 <sajalsatsangi2004@gmail.com>
2024-02-12 22:33:49 +0530

    Education Completed

---
+++ src/components/Education/timelineElements.js
@@ -0,0 +1,54 @@
+let timelineElements = [
+  {
+    id: 6,
+    title: "Bachelor of Technology - Mechanical(CS-Specialization)",
+    location: "Dayalbagh Educational Institute",
+    date: "2021 - Present",
+    icon: "school",
+    cgpa: "8.8",
+  },
+  {
+    id: 5,
+    title: "C++ & STL - STCEI",
+    location: "Dayalbagh, Agra",
+    // buttonText: "View Certificate",
+    // buttonLink: "https://drive.google.com/file/d/1cWaWBe4tkANxVGMCqmCOcdbNX1tvDRyW/view?usp=drive_link",
+    date: "July 2023 - August 2023",
+    icon: "work",
+  },
+  {
+    id: 4,
+    title: "Python - Bolt-IoT",
+    buttonText: "View Certificate",
+    buttonLink: "https://drive.google.com/file/d/1WGo1gSXTa1UkLsPewbXsO9XYTfB9dmAm/view",
+    date: "April 2023 - May 2023",
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "C & DSA - STCEI",
+    location: "Dayalbagh, Agra",
+    buttonText: "View Certificate",
+    buttonLink: "https://drive.google.com/file/d/1P5kAYmGpaMG_1E_VCAsweo54xyQL8xiv/view",
+    date: "May 2022 - August 2022",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "Intermediate - PCM",
+    location: "Radhasoami Educational Institute",
+    date: "2019 - 2021",
+    icon: "school",
+    cgpa: "9.5",
+  },
+  {
+    id: 1,
+    title: "High School",
+    location: "Radhasoami Educational Institute",
+    date: "2017 - 2019",
+    icon: "school",
+    cgpa: "9.0",
+  },
+];
+
+export default timelineElements;
