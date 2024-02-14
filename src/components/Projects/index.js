aba6320accb3952413723ebb6586ca10efe022c5
rashmi-singh2003 <sajalsatsangi2004@gmail.com>
2024-02-14 20:26:30 +0530

    started project page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,7 +3,13 @@ import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import ParticlesBackground from "../Layout/ParticleBackground";
 import styled, { keyframes } from 'styled-components';
-import { fadeIn, fadeInUp, fadeInUpBig, rubberBand } from 'react-animations';
+import { fadeIn } from 'react-animations';
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCards from "./projectCards";
+import { projects } from './data/constants'
+import './index.css'
+import ProjectDetails from './ProjectDetails'
+import './default'
 
 const bounceAnimation = keyframes`${fadeIn}`;
 
@@ -11,8 +17,9 @@ const BouncyDiv = styled.div`
   animation: 1s ${bounceAnimation};
 `;
 
-
 const Projects = () => {
+
+    const [openModal, setOpenModal] = useState({ state: false, project: null });
 
     const [letterClass, setLetterClass] = useState('text-animate');
     const [showTimeline, setShowTimeline] = useState(false);
@@ -37,6 +44,8 @@ const Projects = () => {
         };
     }, []);
 
+    const [toggle, setToggle] = useState('all');
+
     return (
         <>
             <div className="exp-page" >
@@ -49,11 +58,59 @@ const Projects = () => {
                             idx={15}
                         />
                     </h1>
+                    <p>
+                        I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
+                    </p>
                 </div>
             </div>
-            {showTimeline && <BouncyDiv className="Resume-div">
-                {/* <img className="resumeimg" src={LogoImage} alt="Logo" /> */}
+            {showTimeline && <BouncyDiv className="Project-div">
+
+                <Container id="projects">
+                    <Wrapper>
+                        <ToggleButtonGroup >
+                            {toggle === 'all' ?
+                                <ToggleButton className="Togglebutton2" active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+                                :
+                                <ToggleButton className="Togglebutton" value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+                            }
+                            <Divider className="Divider" />
+                            {toggle === 'web app' ?
+                                <ToggleButton className="Togglebutton2" active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+                                :
+                                <ToggleButton className="Togglebutton" value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+                            }
+                            <Divider className="Divider" />
+                            {toggle === 'android app' ?
+                                <ToggleButton className="Togglebutton2" active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+                                :
+                                <ToggleButton className="Togglebutton" value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+                            }
+                            <Divider className="Divider" />
+                            {toggle === 'machine learning' ?
+                                <ToggleButton className="Togglebutton2" active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+                                :
+                                <ToggleButton className="Togglebutton" value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+                            }
+                        </ToggleButtonGroup>
+                        <div  className="Project-div-2">
+                            <CardContainer>
+                                {toggle === 'all' && projects
+                                    .map((project) => (
+                                        <ProjectCards project={project} openModal={openModal} setOpenModal={setOpenModal} />
+                                    ))}
+                                {projects
+                                    .filter((item) => item.category == toggle)
+                                    .map((project) => (
+                                        <ProjectCards project={project} openModal={openModal} setOpenModal={setOpenModal} />
+                                    ))}
+                            </CardContainer>
+                        </div>
+                    </Wrapper>
+                </Container>
             </BouncyDiv>}
+            {openModal.state &&
+                <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+            }
             <Loader type="pacman" />
         </>
     );
