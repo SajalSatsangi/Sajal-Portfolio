785b5eea0ac4775103510c80f7f338384b57543a
rashmi-singh2003 <sajalsatsangi2004@gmail.com>
2024-02-20 22:47:44 +0530

    commit

--- src/components/Projects/data/constants.js
+++ src/components/Projects/data/constants.js
@@ -1,19 +1,121 @@
+import legallink from '../../../assets/images/legal-link-india.png'
+
 export const projects = [
   {
-    id: 9,
+    id: 3,
+    title: "Legal-Link-India",
+    date: "Jul 2023 - Oct 2023",
+    description:
+      "I played a key role in designing the user interface (UI) for Legal-Link-India, an innovative e-marketplace that is reshaping the legal service landscape in India. This cutting-edge online platform serves as a bridge, connecting individuals and businesses with a diverse network of trusted legal professionals. From advocates and arbitrators to mediators, notaries, document writers, and beyond, the platform offers a comprehensive array of legal services. The UI design focuses on creating a seamless and intuitive user experience, ensuring that users can easily navigate the platform and access the legal expertise they need with efficiency and confidence. Legal-Link-India stands at the forefront of transforming the legal service industry by providing a user-friendly and accessible digital marketplace for legal professionals and clients alike.",
+    image:
+      legallink,
+    tags: [
+      "Figma",
+    ],
+    category: ["UI Designing"],
+    github: "https://www.figma.com/proto/obwNwL8laECI2SI9lAqadn/SIH2k23?page-id=0%3A1&node-id=262-315&starting-point-node-id=262%3A315&mode=design&t=oGRvLzANWSD37NEQ-1",
+    webapp: "https://www.figma.com/proto/obwNwL8laECI2SI9lAqadn/SIH2k23?page-id=0%3A1&node-id=262-315&starting-point-node-id=262%3A315&mode=design&t=oGRvLzANWSD37NEQ-1",
+    member: [
+      {
+        name: "Sajal Satsangi",
+        img: "https://avatars.githubusercontent.com/u/119046714?v=4",
+        linkedin: "https://www.linkedin.com/in/sajal-satsangi-547802204/",
+        github: "https://github.com/SajalSatsangi",
+      },
+      {
+        name: "Kanishka Pathik",
+        img: "https://avatars.githubusercontent.com/u/100354245?v=4",
+        linkedin: "https://www.linkedin.com/in/kanishka-pathik-183528243/",
+        github: "https://github.com/Kanishkap1508",
+      },
+      {
+        name: "N Shikhar",
+        img: "https://avatars.githubusercontent.com/u/92314161?v=4",
+        linkedin: "https://www.linkedin.com/in/shikhar-n-a8ab80222/",
+        github: "https://github.com/shikharN007",
+      },
+      {
+        name: "Nihal Saran",
+        img: "https://avatars.githubusercontent.com/u/92871743?v=4",
+        linkedin: "https://www.linkedin.com/in/nihal-saran-das-duggirala/",
+        github: "https://github.com/nihalsaran",
+      },
+      {
+        name: "Madhurie Kumar Seth",
+        img: "https://avatars.githubusercontent.com/u/124090725?v=4",
+        linkedin: "https://www.linkedin.com/in/madhurie-kumar-seth-446228212/",
+        github: "https://github.com/Madhurie12",
+      },
+      {
+        name: "Amiradha Satsangi",
+        img: "https://avatars.githubusercontent.com/u/92226089?v=4",
+        linkedin: "https://www.linkedin.com/in/amiradha-satsangi-6a6234200/",
+        github: "https://github.com/Amiradha",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Exo-Planet Predictor",
+    date: "Aug 2023",
+    description:
+      "I took the lead in developing the frontend of the Exo-Planet Predictor website, where the focus was on integrating a robust machine learning model. This model is designed to predict the habitability of exoplanets by analyzing a diverse set of features, including mass, radius, and orbital characteristics. The frontend of the website was crafted to offer users an intuitive and engaging experience, allowing them to input relevant data and receive insightful predictions about the potential habitability of exoplanets. The combination of a user-friendly interface and a sophisticated machine learning model positions Exo-Planet Predictor as a valuable tool for enthusiasts, researchers, and anyone interested in exploring the possibilities of habitable exoplanets.",
+    image:
+      "https://github.com/SajalSatsangi/Exoplanet-Prediction/blob/main/Screenshot%20(363).png?raw=true",
+    tags: [
+      "HTML", "CSS", "JavaScript", "Machine Learning"
+    ],
+    category: ["web app"],
+    github: "https://github.com/SajalSatsangi/Exoplanet-Prediction",
+    webapp: "https://sajalsatsangi.github.io/Exoplanet-Prediction/",
+    member: [
+      {
+        name: "Sajal Satsangi",
+        img: "https://avatars.githubusercontent.com/u/119046714?v=4",
+        linkedin: "https://www.linkedin.com/in/sajal-satsangi-547802204/",
+        github: "https://github.com/SajalSatsangi",
+      },
+      {
+        name: "N Shikhar",
+        img: "https://avatars.githubusercontent.com/u/92314161?v=4",
+        linkedin: "https://www.linkedin.com/in/shikhar-n-a8ab80222/",
+        github: "https://github.com/shikharN007",
+      },
+      {
+        name: "Nihal Saran",
+        img: "https://avatars.githubusercontent.com/u/92871743?v=4",
+        linkedin: "https://www.linkedin.com/in/nihal-saran-das-duggirala/",
+        github: "https://github.com/nihalsaran",
+      },
+      {
+        name: "Madhurie Kumar Seth",
+        img: "https://avatars.githubusercontent.com/u/124090725?v=4",
+        linkedin: "https://www.linkedin.com/in/madhurie-kumar-seth-446228212/",
+        github: "https://github.com/Madhurie12",
+      },
+      {
+        name: "Amiradha Satsangi",
+        img: "https://avatars.githubusercontent.com/u/92226089?v=4",
+        linkedin: "https://www.linkedin.com/in/amiradha-satsangi-6a6234200/",
+        github: "https://github.com/Amiradha",
+      },
+    ],
+  },
+  {
+    id: 2,
     title: "DEI,FOE",
-    date: "Jun 2023 - Jul 2023",
+    date: "Jan 2024 - Feb 2024",
     description:
       "I spearheaded the creation of a dedicated webpage for the Faculty of Engineering on our college's website, taking charge of both frontend development and UI design. The page showcases a seamless blend of aesthetics and functionality, ensuring an engaging user experience. Through meticulous design and implementation, I aimed to enhance the online presence of the Faculty of Engineering, providing an intuitive and visually appealing platform that aligns with the college's standards of excellence.",
     image:
-      "https://github.com/SajalSatsangi/SecureTech-AV/blob/main/img/Screenshot%20(358).png",
+      "https://github.com/SajalSatsangi/DEIpagedetd/blob/main/img/image.png?raw=true",
     tags: [
       "HTML",
       "CSS",
       "JavaScript",
       "SCSS",
     ],
-    category: ["web app", "UI Designing"],
+    category: ["web app"],
     github: "https://github.com/pri007shu/DEIpagedetd",
     webapp: "https://pri007shu.github.io/DEIpagedetd/",
     member: [
@@ -38,7 +140,7 @@ export const projects = [
     ],
   },
   {
-    id: 11,
+    id: 1,
     title: "SecureTech-AV",
     date: "Oct 2023 - Dec 2023",
     description:
